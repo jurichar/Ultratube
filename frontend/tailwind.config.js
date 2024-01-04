2947aa1a3f92ffb3fe6dbb47fbb773f53fe1d002
Julien Richard <45456851+jurichar@users.noreply.github.com>
2024-01-04 19:33:25 +0100

    Config tailwindcss

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,13 @@
+// frontend/tailwind.config.js
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
