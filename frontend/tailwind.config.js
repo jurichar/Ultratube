801dd19c3b2e13fa06eadbffafd077b78633eaec
Julien Richard <45456851+jurichar@users.noreply.github.com>
2024-01-06 16:05:49 +0100

    Update Dockerfile and Tailwind CSS configuration

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,7 +7,23 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#10141E",
+        secondary: "#FC4747",
+        tertiary: "#161D2F",
+        quaternary: "#5A698F",
+        quinary: "#FFFFFF",
+      },
+      fontSize: {
+        'heading-lg': ['32px', { lineHeight: '1' }],
+        'heading-md': ['24px', { lineHeight: '1' }],
+        'heading-sm': ['18px', { lineHeight: '1' }],
+        'heading-xs': ['13px', { lineHeight: '1' }],
+        'body-md': ['15px', { lineHeight: '1.5' }],
+        'body-sm': ['13px', { lineHeight: '1.5' }],
+      },
+    },
   },
   plugins: [],
 }
