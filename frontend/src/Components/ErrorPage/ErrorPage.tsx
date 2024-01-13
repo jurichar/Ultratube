cd20be2cb3cdabbc4b12451a98012d5aedbf533b
Julien Richard <45456851+jurichar@users.noreply.github.com>
2024-01-14 00:45:23 +0100

    Refactor ErrorPage component

--- frontend/src/Components/ErrorPage/ErrorPage.tsx
+++ frontend/src/Components/ErrorPage/ErrorPage.tsx
@@ -1,9 +1,18 @@
-export default function ErrorPage() {
+// src/Components/ErrorPage/ErrorPage.tsx
+
+import React from "react";
+
+const ErrorPage: React.FC<{ errorCode?: number }> = ({ errorCode = 404 }) => {
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p></p>
+    <div className="flex flex-col items-center justify-center h-screen bg-primary">
+      <h2 className="text-5xl font-bold text-quaternary mb-4">{`Error ${errorCode}`}</h2>
+      <h3 className="text-3xl font-semibold text-secondary">Page not found</h3>
+      <button className="mt-4 bg-tertiary text-quaternary px-4 py-2 rounded hover:opacity-50 transition-all"
+        onClick={() => window.location.href = "/"}>
+        Go back home
+      </button>
     </div>
   );
-}
+};
+
+export default ErrorPage;
