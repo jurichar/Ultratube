deed262163c2b5503e0f96a4075b92972f6027e4
macrespo42 <macrespo@student.42.fr>
2024-01-15 18:17:57 +0100

    fix: make a separate subtitle Table

--- backend/movie/models.py
+++ backend/movie/models.py
@@ -12,18 +12,20 @@ class Movie(models.Model):
     genre = models.CharField(max_length=255)
     imdb_rating = models.FloatField(null=True)
     peer = models.IntegerField()  # to sort movies can also be downloaded or seeders
-    subtitles = ArrayField(
-        models.CharField(max_length=255, null=True)
-    )
     casting = ArrayField(
         models.CharField(max_length=64)
     )
 
 
+class Subtitle(models.Model):
+    location = models.CharField(max_length=255)
+    language = models.CharField(max_length=5)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+
+
 class WatchedMovie(models.Model):
     watcher = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
-    favourite = models.BooleanField(default=False)
     watched_at = models.DateTimeField(auto_now_add=True)
 
 
@@ -32,3 +34,8 @@ class Comment(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+class FavouriteMovie(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
