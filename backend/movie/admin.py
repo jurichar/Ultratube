698e03fe9e22ba8f66fcc96222efbd27af7d1aa4
macrespo42 <macrespo@student.42.fr>
2024-01-15 15:18:29 +0100

    feat: add models to admin site - fix flake8 problems generated by django startapp

--- backend/movie/admin.py
+++ backend/movie/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Comment, Movie, WatchedMovie
+
 # Register your models here.
+admin.site.register(Comment)
+admin.site.register(Movie)
+admin.site.register(WatchedMovie)
