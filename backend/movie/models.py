d1ddcc8aab90b26a9b84f4296d37d4ba45c5f9ee
macrespo42 <macrespo@student.42.fr>
2024-01-16 14:04:20 +0100

    feat: unit test for movies pass

--- backend/movie/models.py
+++ backend/movie/models.py
@@ -6,22 +6,28 @@ from django.db import models
 
 class Movie(models.Model):
     name = models.CharField(max_length=128, unique=True)
-    synopsis = models.TextField(null=True)
+    synopsis = models.TextField(null=True, blank=True)
     thumbnail_cover = models.CharField(max_length=255)
-    production_year = models.IntegerField(validators=[MinValueValidator(1888)], null=True)
+    production_year = models.IntegerField(validators=[MinValueValidator(1888)], null=True, blank=True)
     duration = models.DurationField()
     genre = models.CharField(max_length=255)
-    imdb_rating = models.FloatField(validators=[MinValueValidator(0.0)], null=True)
+    imdb_rating = models.FloatField(validators=[MinValueValidator(0.0)], null=True, blank=True)
     peer = models.IntegerField(validators=[MinValueValidator(0)])  # to sort movies can also be downloaded or seeders
     casting = ArrayField(
         models.CharField(max_length=64)
     )
+
+    def __str__(self):
+        return self.name
 
 
 class Subtitle(models.Model):
     location = models.CharField(max_length=255)
     language = models.CharField(max_length=5)
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.language}: {self.movie__name}"
 
 
 class WatchedMovie(models.Model):
@@ -35,6 +41,9 @@ class Comment(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.content
 
 
 class FavouriteMovie(models.Model):
--- backend/movie/tests.py
+++ backend/movie/tests.py
@@ -1,3 +1,97 @@
-# from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.core.validators import ValidationError
+from django.test import TestCase
+
+from .models import Movie
+
+
+class MovieTestCase(TestCase):
+    def setUp(self) -> None:
+        Movie.objects.create(
+            name="Return of the Jedi",
+            thumbnail_cover="path/to/thumbnail/",
+            duration=datetime.timedelta(days=20, hours=10),
+            genre="epic space opera",
+            peer=0,
+            casting=["Georges Lucas", "Mark Hamill"],
+        )
+
+    def test_movie_str(self):
+        movie = Movie.objects.first()
+        self.assertEqual(str(movie), movie.name)
+
+    def test_negative_imdb_rating(self):
+        movie = Movie(
+            name="A New Hope",
+            thumbnail_cover="path/to/thumbnail/",
+            duration=datetime.timedelta(days=20, hours=10),
+            genre="epic space opera",
+            peer=0,
+            casting=["Georges Lucas", "Mark Hamill"],
+            imdb_rating=-42.0,
+        )
+
+        with self.assertRaises(ValidationError):
+            movie.full_clean()
+            movie.save()
+
+    def test_negative_peer(self):
+        movie = Movie(
+            name="A New Hope",
+            thumbnail_cover="path/to/thumbnail/",
+            duration=datetime.timedelta(days=20, hours=10),
+            genre="epic space opera",
+            casting=["Georges Lucas", "Mark Hamill"],
+            peer=-42,
+        )
+
+        with self.assertRaises(ValidationError):
+            movie.full_clean()
+            movie.save()
+
+    def test_bad_production_year(self):
+        movie = Movie(
+            name="A New Hope",
+            thumbnail_cover="path/to/thumbnail/",
+            duration=datetime.timedelta(days=20, hours=10),
+            genre="epic space opera",
+            casting=["Georges Lucas", "Mark Hamill"],
+            peer=0,
+            production_year=1777
+        )
+
+        with self.assertRaises(ValidationError):
+            movie.full_clean()
+            movie.save()
+
+    def test_create_movie_with_minimum_info(self):
+        movie = Movie(
+            name="A New Hope",
+            thumbnail_cover="path/to/thumbnail/",
+            duration=datetime.timedelta(days=20, hours=10),
+            genre="epic space opera",
+            casting=["Georges Lucas", "Mark Hamill"],
+            peer=0,
+        )
+        movies_count_before_create = Movie.objects.all().count()
+        movie.full_clean()
+        movie.save()
+        self.assertEqual(movies_count_before_create + 1, Movie.objects.all().count())
+
+    def test_create_movie_with_full_infos(self):
+        movie = Movie(
+            name="A New Hope",
+            thumbnail_cover="path/to/thumbnail/",
+            duration=datetime.timedelta(days=20, hours=10),
+            genre="epic space opera",
+            casting=["Georges Lucas", "Mark Hamill"],
+            peer=0,
+            synopsis="Lorem ipsum",
+            production_year=1977,
+            imdb_rating=9.3
+        )
+        movies_count_before_create = Movie.objects.all().count()
+        movie.full_clean()
+        movie.save()
+        self.assertEqual(movies_count_before_create + 1, Movie.objects.all().count())
