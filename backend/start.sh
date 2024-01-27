f417e466533e649c8cfe16340c2833b0bf3553d5
mpochard <88907375+m3L4n@users.noreply.github.com>
2024-01-27 15:39:14 +0100

    "fix : migration pb"

--- backend/start.sh
+++ backend/start.sh
@@ -3,5 +3,5 @@
 ./manage.py makemigrations
 ./manage.py migrate
 ./manage.py init_local_dev 
-./manage.py init_local_dev_auth 
+# ./manage.py init_local_dev_auth 
 ./manage.py runserver 0.0.0.0:8000
