e17240bbfdf351a2101b58c4e5fb0c570f7b5102
mpochard <88907375+m3L4n@users.noreply.github.com>
2024-01-27 18:00:54 +0100

    'coch: fix set_auth"

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -41,8 +41,10 @@ INSTALLED_APPS = [
     "rest_framework_swagger",
     "rest_framework",
     "movie",
+    "authentication",
 ]
 
+AUTH_USER_MODEL = "authentication.User"
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
