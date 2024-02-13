6eecd2d613d9f9617dc5417eeb5324d62a622120
macrespo42 <macrespo@student.42.fr>
2024-02-13 23:39:58 +0100

    feat: get torrentUrl

--- streaming_service/src/index.ts
+++ streaming_service/src/index.ts
@@ -1,1 +1,20 @@
-console.log("Hello world!");
+async function downloadTorrent(torrentUrl: string): Promise<ArrayBuffer> {
+  try {
+    const response = await fetch(torrentUrl);
+
+    if (!response.ok) {
+      console.log("Error when downloading .torrent");
+    }
+
+    const torrentData = await response.arrayBuffer();
+
+    return torrentData;
+  } catch (e) {
+    console.log(e.message);
+  }
+}
+
+const torrentUrl: string =
+  "https://torrents.yts.rs/torrent/download/CDC5A3E045636502C8D4485E547B2D45D3F2DF41";
+
+console.log(await downloadTorrent(torrentUrl));
