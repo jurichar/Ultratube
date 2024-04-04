7269929556c62001bed305ea80dc6598843b3ff9
macrespo42 <macrespo@student.42.fr>
2024-04-04 17:19:32 +0200

    chore: streaming work with one reload

--- streaming_service/src/bittorent-client/metadataHandler.ts
+++ streaming_service/src/bittorent-client/metadataHandler.ts
@@ -1,65 +1,10 @@
 "use strict";
 import fs from "node:fs/promises";
-import https from "https";
-import bencode from "bencode";
-import * as ttypes from "./ft_torrent_types.js";
-import parseTorrent from "parse-torrent";
 import ParseTorrent from "parse-torrent";
 
 function generatePath(torrentUrl: string): string {
   const splitedUrl: string[] = torrentUrl.split("/");
   return `./torrents/metadata/${splitedUrl[splitedUrl.length - 1].toLowerCase()}`;
-}
-
-function normalizeTorrentMeta(
-  decodedMetadata: any,
-  originalMetaData: any,
-): ttypes.TorrentMeta {
-  const torrentMetaData = {} as ttypes.TorrentMeta;
-  const info = {} as ttypes.Info;
-
-  // Determine if the .torrent file have one or multiples files
-  if (Object.prototype.hasOwnProperty.call(decodedMetadata?.info, "length")) {
-    const file: ttypes.SingleFileMode = {
-      name: decodedMetadata?.info?.name,
-      length: decodedMetadata?.info?.length,
-      md5sum: decodedMetadata?.info?.md5sum,
-    };
-
-    info.file = file;
-  } else if (
-    Object.prototype.hasOwnProperty.call(decodedMetadata?.info, "files")
-  ) {
-    const file: ttypes.MultiFileMode = {
-      name: decodedMetadata?.info?.name,
-      files: decodedMetadata?.info?.files.map((rawFile: any) => ({
-        length: rawFile?.length,
-        path: rawFile?.length,
-        md5sum: rawFile?.md5sum,
-      })),
-    };
-    info.file = file;
-  } else {
-    throw new Error("Torrent type unhandled");
-  }
-
-  info.pieceLength = decodedMetadata?.info["piece length"];
-  info.pieces = originalMetaData.pieces;
-  info.piecesBuffer = originalMetaData.info.pieces;
-  info.private =
-    parseInt(decodedMetadata?.info?.private, 10) === 1 ? true : false;
-
-  torrentMetaData.announce = decodedMetadata.announce;
-  torrentMetaData.announceList = decodedMetadata["announce-list"];
-
-  torrentMetaData.createdBy = decodedMetadata["created by"];
-  torrentMetaData.creationDate = decodedMetadata["creation date"];
-  torrentMetaData.encoding = decodedMetadata?.encoding;
-  torrentMetaData.comment = decodedMetadata?.comment;
-  torrentMetaData.info = info;
-  torrentMetaData.infoHash = originalMetaData.infoHashBuffer;
-
-  return torrentMetaData;
 }
 
 export async function downloadTorrentMeta(torrentUrl: string) {
@@ -72,7 +17,6 @@ export async function downloadTorrentMeta(torrentUrl: string) {
 
 export async function parseTorrentMeta(torrentPath: string) {
   const torrent = await fs.readFile(torrentPath);
-  console.log("My length boy: ", torrent.length);
   try {
     const metadata = ParseTorrent(torrent);
     return metadata;
--- streaming_service/src/index.ts
+++ streaming_service/src/index.ts
@@ -1,5 +1,6 @@
 "use strict";
 import express from "express";
+import path from "node:path";
 const app = express();
 
 import {
@@ -9,46 +10,76 @@ import {
   generateMagnetURI,
 } from "./bittorent-client/metadataHandler.js";
 
-// import torrentStream from "torrent-stream";
-// import fs from "node:fs";
+import torrentStream from "torrent-stream";
+import fs from "node:fs";
 
 app.get("/stream/:movieId", async (request, response) => {
   const { movieId } = request.params as {
     movieId: string;
   };
 
+  console.log(movieId);
+
   const torrentUrl = "https://webtorrent.io/torrents/big-buck-bunny.torrent";
   const torrentPath = await downloadTorrentMeta(torrentUrl);
   const torrentMetaData = await parseTorrentMeta(torrentPath);
   const magnetURI = generateMagnetURI(torrentMetaData, torrentUrl);
 
-  console.log(magnetURI);
+  const torrentDir = `${path.resolve()}/torrents/${torrentMetaData.name}`;
+  try {
+    await fs.promises.mkdir(torrentDir);
+  } catch (error) {
+    console.error("THIS IS AN ERROR LOL: ", error.message);
+  }
 
   await deleteTorrentMeta(torrentPath);
-  //
-  // const range = request.headers.range;
-  // if (!range) {
-  //   return response.status(400).send("Require Range header");
-  // } else {
-  //   const path = `./torrents/video.mp4`;
-  //   const videoSize = fs.statSync(path).size;
-  //   const chunkSize = 10 ** 6;
-  //   const start = Number(range.replace(/\D/g, ""));
-  //   const end = Math.min(start + chunkSize, videoSize - 1);
-  //   const contentLength = end - start + 1;
-  //   const headers = {
-  //     "Content-Range": `bytes ${start}-${end}/${videoSize}`,
-  //     "Accept-Ranges": "bytes",
-  //     "Content-Length": contentLength,
-  //     "Content-Type": "video/mp4",
-  //   };
-  //
-  //   const videoStream = fs.createReadStream(path, { start, end });
-  //
-  //   response.writeHead(206, headers);
-  //   return videoStream.pipe(response);
-  // }
-  response.status(200).send(movieId);
+
+  const engine = torrentStream(magnetURI, { path: "./torrents/" });
+  const range = request.headers.range;
+  let video: TorrentStream.TorrentFile;
+
+  engine.on("ready", () => {
+    engine.files.forEach((file) => {
+      if (file.name.endsWith("mp4")) {
+        file.select();
+        if (!range) {
+          response.status(400).send("Require Range header");
+        }
+        fs.promises
+          .writeFile(path.join(torrentDir, file.name), "", { flag: "a+" })
+          .then(() => {
+            video = file;
+            const videoSize = video.length;
+            const chunkSize = 10 ** 6;
+            const start = Number(range.replace(/\D/g, ""));
+            const end = Math.min(start + chunkSize, videoSize - 1);
+            const contentLength = end - start + 1;
+            const headers = {
+              "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+              "Accept-Ranges": "bytes",
+              "Content-Length": contentLength,
+              "Content-Type": "video/mp4",
+            };
+
+            const videoStream = fs.createReadStream(
+              path.join(torrentDir, file.name),
+              { start, end },
+            );
+
+            response.writeHead(206, headers);
+            videoStream.pipe(response);
+          })
+          .catch((err) => {
+            console.log("WSH WSH WSH");
+            console.log(err);
+          });
+      } else {
+        file.deselect();
+      }
+    });
+  });
+
+  engine.on("download", () => {});
 });
 
 app.listen(8001, async () => {
