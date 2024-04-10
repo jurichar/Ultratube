14d58ba91e3619c547fc4b11e972c8f1725f62b3
macrespo42 <macrespo@student.42.fr>
2024-04-10 17:32:38 +0200

    feat: convert video when streaming if needed

--- streaming_service/src/index.ts
+++ streaming_service/src/index.ts
@@ -9,37 +9,58 @@ import {
   generateMagnetURI,
 } from "./bittorent-client/metadataHandler.js";
 
-import {
-  TORRENT_PATH,
-  downloadMovie,
-  getStreamingHeaders,
-} from "./streaming.js";
+import { TORRENT_PATH, downloadMovie } from "./streaming.js";
 
 import fs from "node:fs";
 import path from "node:path";
+import pump from "pump";
+import ffmpeg from "fluent-ffmpeg";
 
 app.get("/stream", async (request, response) => {
-  const torrentUrl = "https://webtorrent.io/torrents/big-buck-bunny.torrent";
-  const torrentPath = await downloadTorrentMeta(torrentUrl);
-  const torrentMetaData = await parseTorrentMeta(torrentPath);
-  const magnetURI = generateMagnetURI(torrentMetaData, torrentUrl);
-
-  await deleteTorrentMeta(torrentPath);
-
-  const videoFile = await downloadMovie(magnetURI);
-
   const range = request.headers.range;
   if (!range) {
     response.status(400).send("range header required");
   }
-  const headers = getStreamingHeaders(range, videoFile);
+
+  const torrentUrl = "https://webtorrent.io/torrents/big-buck-bunny.torrent";
+  const torrentPath = await downloadTorrentMeta(torrentUrl);
+  const torrentMetaData = await parseTorrentMeta(torrentPath);
+  const magnetURI = generateMagnetURI(torrentMetaData, torrentUrl);
+  await deleteTorrentMeta(torrentPath);
+
+  const videoFile = await downloadMovie(magnetURI, torrentMetaData.announce);
+  const ismp4 = videoFile.name.endsWith(".mp4");
+
+  const CHUNK_SIZE = 10 ** 6;
+  const start = Number(range.replace(/\D/g, ""));
+  const end = Math.min(start + CHUNK_SIZE, videoFile.length - 1);
+
+  const headers = ismp4
+    ? {
+        "Content-Range": `bytes ${start}-${end}/${videoFile.length}`,
+        "Accept-Ranges": "bytes",
+        "Content-Length": end - start + 1,
+        "Content-Type": "video/mp4",
+      }
+    : {
+        "Content-Range": `bytes ${start}-${end}/${videoFile.length}`,
+        "Accept-Ranges": "bytes",
+        "Content-Type": "video/webm",
+      };
 
   const videoStream = fs.createReadStream(
     path.join(TORRENT_PATH, videoFile.path),
+    { start, end },
   );
 
-  response.status(206).set(headers);
-  videoStream.pipe(response);
+  response.writeHead(206, headers);
+  if (ismp4) {
+    console.log("Streaming as mp4...");
+    pump(videoStream, response);
+  } else {
+    console.log("Streaming as webm");
+    ffmpeg(videoStream).format("webm").pipe(response);
+  }
 });
 
 app.listen(8001, async () => {
--- streaming_service/src/streaming.ts
+++ streaming_service/src/streaming.ts
@@ -6,31 +6,44 @@ export const TORRENT_PATH = "./torrents";
 
 export async function downloadMovie(
   magnetURI: string,
+  trackers: string[],
 ): Promise<TorrentStream.TorrentFile> {
   return new Promise((resolve) => {
-    const engine = torrentStream(magnetURI, { path: TORRENT_PATH });
+    const engine = torrentStream(magnetURI, {
+      path: TORRENT_PATH,
+      trackers: trackers,
+    });
     let videoFile: TorrentStream.TorrentFile;
 
     engine.on("ready", () => {
-      const file = engine.files.find((file) => file.name.endsWith(".mp4"));
-      videoFile = file;
-      videoFile.select();
+      engine.files.forEach((file) => {
+        if (file.name.endsWith(".mp4")) {
+          videoFile = file;
+          videoFile.select();
+        } else {
+          file.deselect();
+        }
+      });
     });
 
     engine.on("download", () => {
+      const downloaded =
+        Math.round((engine.swarm.downloaded / videoFile.length) * 100 * 100) /
+        100;
+      console.log("Downloaded: " + downloaded + "%");
+
       const moviePath = path.join(TORRENT_PATH, videoFile.path);
       if (fs.existsSync(moviePath)) {
-        if (fs.statSync(moviePath).size / (1024 * 1024) > 20) {
-          resolve(videoFile);
-        }
+        console.log("Movie path exist: ", moviePath);
+        resolve(videoFile);
       }
     });
 
-    engine.on("idle", () => {
-      engine.destroy(() => {
-        console.log("Video downloaded");
-      });
-    });
+    // engine.on("idle", () => {
+    //   engine.destroy(() => {
+    //     console.log("Video downloaded");
+    //   });
+    // });
   });
 }
 
@@ -38,15 +51,14 @@ export function getStreamingHeaders(
   range: string,
   videoFile: TorrentStream.TorrentFile,
 ) {
-  const chunksize = 20e6;
+  const CHUNK_SIZE = 10 ** 6;
   const start = Number(range.replace(/\D/g, ""));
-  const end = Math.min(start + chunksize, videoFile.length - 1);
-  const contentLength = end - start + 1;
+  const end = Math.min(start + CHUNK_SIZE, videoFile.length - 1);
 
   const headers = {
     "Content-Range": `bytes ${start}-${end}/${videoFile.length}`,
     "Accept-Ranges": "bytes",
-    "Content-Length": contentLength,
+    "Content-Length": end - start + 1,
     "Content-Type": "video/mp4",
   };
 
